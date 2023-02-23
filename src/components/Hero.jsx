d21940d28ea3ae9769ec26db04e1f8e01ac15b3d
Mutesi <ktesialine@gmail.com>
2023-02-23 18:18:12 +0200

    fix:text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero (props){
       <p className="text-[39px] font-bold">Fun facts about React</p>
       <ul className="space-y-3 list-disc marker:text-sky-500 pl-5 text-base">
         <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
+        <li>Was originally created by Jordann Walke</li>
         <li>Has well over 100K stars on GitHub</li>
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
